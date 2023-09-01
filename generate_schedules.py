2596eaad05e11aafb3b6bb46276d424f33a8e575
Koen van der Blom <koen.vdblom@lip6.fr>
2023-09-01 14:27:50 +0200

    Implement schedule generation by game placement with constraint avoidance.

--- generate_schedules.py
+++ generate_schedules.py
@@ -4,10 +4,12 @@ Code adapted from:
 https://github.com/kristianverduin/MSc-Thesis-RandomScheduleGeneration-ViolationReduction-TTP/blob/4a00582f02819f7b75a77cd2d638b068026b6608/violations.py
 """
 
-import numpy as np
 import random
 from datetime import datetime
 import sys
+
+import numpy as np
+import pandas as pd
 
 max_streak = 3
 
@@ -71,9 +73,9 @@ def check_schedule_constraints(schedule: np.array, n_teams: int) -> list[int]:
 
 
 def check_double_round_robin(team: int,
-                             games_played: np.array[int],
-                             home_played: np.array[int],
-                             away_played: np.array[int]) -> int:
+                             games_played: np.array,
+                             home_played: np.array,
+                             away_played: np.array) -> int:
     """Check for double round-robin violations.
 
     Arguments:
@@ -134,9 +136,9 @@ def create_random_schedule_pairs(n_teams: int) -> np.array:
     Returns:
         Schedule ([int, int]) : The randomly generated schedule
     """
-    n_rounds = (2*n_teams)-2
+    n_rounds = (2 * n_teams) - 2
     schedule = np.full((n_rounds, n_teams), None)
-    choices = list(range(-n_teams, n_teams+1))
+    choices = list(range(-n_teams, n_teams + 1))
     choices.remove(0)
 
     for round_n in range(n_rounds):
@@ -159,6 +161,164 @@ def create_random_schedule_pairs(n_teams: int) -> np.array:
                     schedule[round_n, abs(choice)-1] = team
 
     return schedule
+
+
+def create_random_schedule_games(n_teams: int) -> (np.array, bool):
+    """Generate a schedule by randomly assigning feasible games.
+
+    Arguments:
+        n_teams (int) : The number of teams present in the schedule
+
+    Returns:
+        Tuple with:
+            - Schedule ([int, int]) : The randomly generated schedule
+            - True if the schedule is feasible, False if the schedule is
+            infeasible (and therefore incomplete)
+    """
+    n_rounds = (2 * n_teams) - 2
+    schedule = np.full((n_rounds, n_teams), None)
+    games = generate_games_list(n_teams)
+
+    for round_n in range(n_rounds):
+        # Take all games still awaiting assignment
+        games_to_pick = games.copy()
+
+        for team in range(n_teams):
+            if schedule[round_n, team] is None:
+                team += 1
+                # 1. Take all games relevant for this team and round, that
+                # don't violate the constraints.
+                games_relevant = games_to_pick.copy()
+                # a. Remove games that don't involve this team
+                games_relevant.drop(
+                    games_relevant[
+                        (games_relevant.home_teams != team)
+                        & (games_relevant.away_teams != team)].index,
+                    axis=0, inplace=True)
+
+                # b. Remove games that repeat the opponent from last round
+                if round_n > 0:
+                    games_relevant.drop(
+                        games_relevant[
+                            (games_relevant.home_teams
+                             == abs(schedule[round_n-1, team-1]))
+                            | (games_relevant.away_teams
+                               == abs(schedule[round_n-1, team-1]))].index,
+                        inplace=True)
+
+                # c. If max_streak would be exceeded, remove home/away games
+                # involving that team, depending on which streak it is.
+                for player in range(n_teams):
+                    streak_home, streak_away = count_streak(
+                        schedule, player, round_n)
+
+                    if streak_home >= max_streak:
+                        games_relevant.drop(
+                            games_relevant[
+                                games_relevant.home_teams == player].index,
+                            inplace=True)
+                    elif streak_away >= max_streak:
+                        games_relevant.drop(
+                            games_relevant[
+                                games_relevant.away_teams == player].index,
+                            inplace=True)
+
+                # 2. Try to assign a game
+                # a. Check if any options are available, otherwise return early
+                if len(games_relevant.index) == 0:
+                    return (schedule, False)
+
+                # b. Select the game
+                choice = games_relevant.sample().reset_index(drop=True)
+                choice_home = choice.at[0, "home_teams"]
+                choice_away = choice.at[0, "away_teams"]
+
+                # c. Put game in schedule, indicating away with negative
+                schedule[round_n, choice.home_teams-1] = -choice.away_teams
+                schedule[round_n, choice.away_teams-1] = choice.home_teams
+
+                # 3. Remove games from relevant DataFrames to prepare for the
+                # next assignment and/or round.
+                # a. Remove the choice from the games DataFrame
+                games.drop(
+                    games[(games.home_teams == choice_home)
+                          & (games.away_teams == choice_away)].index,
+                    inplace=True)
+
+                # b. Remove all games involving the teams of the choice from
+                # the games_to_pick DataFrame (they cannot be placed again in
+                # this round)
+                games_to_pick.drop(
+                    games_to_pick[
+                        (games_to_pick.home_teams == choice_home)
+                        | (games_to_pick.away_teams == choice_home)
+                        | (games_to_pick.home_teams == choice_away)
+                        | (games_to_pick.away_teams == choice_away)].index,
+                    inplace=True)
+
+    return (schedule, True)
+
+
+def count_streak(schedule: np.array,
+                 team: int,
+                 current_round: int) -> (int, int):
+    """Count the home and away streaks of a team for an incomplete schedule.
+
+    Arguments:
+        schedule: A partial schedule.
+        team: The team to count the streaks for.
+        current_round: The round for which to count the preceding streaks.
+
+    Returns:
+        streak_home: int of the home streak
+        streak_away: int of the away streak
+    """
+    streak_home = 0
+    streak_away = 0
+
+    for round_n in range(current_round - 1, -1, -1):
+        if schedule[round_n, team-1] is None:
+            # If None is found the streak ends here and we stop
+            # NOTE: Could lead to infeasible assignment if relied on when
+            # assigning to random slots in the schedule, rather than from the
+            # top
+            break
+        elif schedule[round_n, team-1] > 0 and streak_away == 0:
+            streak_home += 1
+        elif schedule[round_n, team-1] < 0 and streak_home == 0:
+            streak_away += 1
+        else:
+            # Either a home or away game was in the schedule, while the other
+            # streak was non-zero, so we reached a switching point and stop.
+            break
+
+    return (streak_home, streak_away)
+
+
+def generate_games_list(n_teams: int) -> pd.DataFrame:
+    """Generate a list of games for the given number of teams.
+
+    Arguments:
+        n_teams: The number of teams in the tournament.
+
+    Returns:
+        pd.DataFrame with two columns:
+            home_teams: int of the team number
+            away_teams: int of the team number
+    """
+    home_teams = list()
+    away_teams = list()
+
+    for home_team in range(n_teams):
+        for away_team in [teams for teams in range(n_teams)
+                          if teams != home_team]:
+            home_teams.append(home_team + 1)
+            away_teams.append(away_team + 1)
+
+    games = pd.DataFrame({"home_teams": home_teams,
+                          "away_teams": away_teams})
+
+    return games
 
 
 def create_schedules(n_teams: int, n_schedules: int) -> None:
@@ -214,9 +374,20 @@ def create_schedules(n_teams: int, n_schedules: int) -> None:
 
 
 n_teams = int(sys.argv[1])
-n_schedules = 1000000
+n_schedules = 100000
 
 if n_teams % 2 == 0:
-    create_schedules(n_teams, n_schedules)
+    perfect = 0
+    for i in range(0, n_schedules):
+        schedule, good = create_random_schedule_games(n_teams)
+        if good:
+            # print(schedule)
+            constraints = check_schedule_constraints(schedule, n_teams)
+            print(f"{i}: {constraints}")
+            if sum(constraints) == 0:
+                perfect += 1
+    print(
+        f"n_teams: {n_teams}, n_schedules: {n_schedules}, perfect: {perfect}")
+    # create_schedules(n_teams, n_schedules)
 else:
     print(f"Number of teams must be even, but was {n_teams}.")
